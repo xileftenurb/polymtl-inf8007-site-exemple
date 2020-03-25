26638b659eb66122fe5aa3d096451241fca3c28e
Félix Brunet <xileftenurb@hotmail.com>
2020-03-25 14:55:26 -0400

    adding some link and page in the website to test scrapper and crawler

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'index' });
 });
+
+router.get('/page2', function(req, res, next) {
+    res.render('page2', { title: 'page2' });
+});
+
+router.get('/page3', function(req, res, next) {
+    res.render('index', { title: 'page3' });
+});
+
+router.get('/page4', function(req, res, next) {
+    res.render('index', { title: 'page4' });
+});
+
+router.get('/page5', function(req, res, next) {
+    res.render('index', { title: 'page4' });
+});
+
+router.get("/errored-page", function(req, res, next) {
+    res.status(500);
+    res.end();
+});
+
+router.get("/private-page", function(req, res, next) {
+    res.status(401);
+    res.json({message : "your are not authentified"})
+})
+
+router.get("/permanent-redirect-to-root-page", function(req, res, next) {
+    res.redirect(301, "/")
+})
+
+router.get("/temporary-redirect-to-error-page", function(req, res, next) {
+    res.redirect(302, "/errored-page")
+});
+
+router.get("/created", function(req, res, next) {
+    res.status(201);
+    res.render("index", {title : 'created'});
+});
+
 
 module.exports = router;
